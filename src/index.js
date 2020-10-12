c9296abf26962e03c7feaacb396f2c0213d68118
Diana <dianavaleeva@yahoo.com>
2020-10-12 20:48:28 +0300

    init: add reverse int solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let str = "";
+    n < 0 ? str = "" + n * -1 : str = "" + n;
+    return str.split("").reverse().join("");
 }
